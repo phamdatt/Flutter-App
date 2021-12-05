00f6cf453d855db1be56d52800ba8d1b8d003910
phamdatt3009 <tiendatt3009@gmail.com>
2021-12-06 00:44:14 +0700

    dev-dat-push

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class MyApp extends StatefulWidget {
-  String name;
-  int age;
-  MyApp({required this.name, required this.age});
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -12,45 +8,55 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _email = ''; //this is state
-  final emailEditingController = TextEditingController();
-  DateTime now = new DateTime.now();
+  final EmailController = TextEditingController();
+  String _email = '';
+  int count = 0;
+  incree() {
+    this.setState(() {
+      count++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-        title: 'This is statefullWidget',
+        title: 'This is test',
         home: Scaffold(
-            body: Center(
+            body: SafeArea(
                 child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              child: TextField(
-                controller: emailEditingController,
-                onChanged: (text) {
-                  this.setState(() {
-                    _email = text;
-                  });
-                },
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
+                child: TextField(
+              controller: EmailController,
+              onChanged: (text) {
+                this.setState(() {
+                  _email = text;
+                });
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
                       borderRadius:
-                          const BorderRadius.all(const Radius.circular(20)),
-                    ),
-                    labelText: "Email"),
-              ),
-            ),
-            Text(
-              "asdasd",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 20, color: Colors.red),
-            ),
+                          const BorderRadius.all(Radius.circular(20))),
+                  labelText: "Enter your email"),
+            )),
             Text(
               _email,
               style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 20, color: Colors.red),
+                  fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
               textDirection: TextDirection.ltr,
+            ),
+            Text(
+              '$count',
+              style: TextStyle(fontSize: 20),
+              textDirection: TextDirection.ltr,
+            ),
+            TextButton(
+              child: Text('Incree'),
+              onPressed: () {
+                incree();
+              },
             )
           ],
         ))));
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,5 @@ import 'package:flutter/material.dart';
 import 'MyApp.dart';
 
 void main() {
-  runApp(MyApp(
-    name: 'Pham Tien Dat',
-    age: 23,
-  ));
+  runApp(MyApp());
 }
